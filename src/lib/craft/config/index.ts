5b0c5769bf85dfc2ee108f74088cf9a1ad9f7d5b
Omhet <omhetehmo@yandex.ru>
2025-04-19 23:33:47 +0200

    feat(crafting): Enhance loot configuration for blade weapons with detailed recipes and parts

--- src/lib/craft/config/index.ts
+++ src/lib/craft/config/index.ts
@@ -1,8 +1,32 @@
-import { LootConfig } from "../craftModel.js";
+import {
+    LootConfig,
+    PecipePartType,
+    RecipeDetailType,
+    RecipeItemType,
+} from "../craftModel.js";
+import { bladeWeaponHilt } from "./bladeWeapon/parts.js";
+import {
+    basicShortSwordBladeDetail,
+    basicShortSwordGrip,
+    basicShortSwordGuard,
+    basicShortSwordPommel,
+} from "./bladeWeapon/shortSword/basicShortSwordDetails.js";
+import { shortSwordBlade } from "./bladeWeapon/shortSword/parts.js";
+import { shortSwordRecipe } from "./bladeWeapon/shortSword/recipe.js";
 
 // Assemble the final LootConfig object
 export const lootConfig: LootConfig = {
-    recipeItems: {},
-    recipeParts: {},
-    recipeDetailVariants: {},
+    recipeItems: {
+        [RecipeItemType.BladeWeapon]: [shortSwordRecipe],
+    },
+    recipeParts: {
+        [PecipePartType.BladeWeaponHilt]: [bladeWeaponHilt],
+        [PecipePartType.ShortSwordBlade]: [shortSwordBlade],
+    },
+    recipeDetailVariants: {
+        [RecipeDetailType.Pommel]: [basicShortSwordPommel],
+        [RecipeDetailType.Grip]: [basicShortSwordGrip],
+        [RecipeDetailType.Guard]: [basicShortSwordGuard],
+        [RecipeDetailType.ShortSwordBlade]: [basicShortSwordBladeDetail],
+    },
 };
