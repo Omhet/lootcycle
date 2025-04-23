98b045cfa5107f16e7faaa2e6d248d4e10682efc
Omhet <omhetehmo@yandex.ru>
2025-04-23 18:18:39 +0200

    feat: Add intake_filled asset and update Preloader to load new assets

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -1,109 +1,98 @@
 import { Scene } from "phaser";
 
 export class Preloader extends Scene {
-    container: Phaser.GameObjects.Container;
+  container: Phaser.GameObjects.Container;
 
-    constructor() {
-        super("Preloader");
-    }
+  constructor() {
+    super("Preloader");
+  }
 
-    init() {
-        const centerX = this.cameras.main.width / 2;
-        const centerY = this.cameras.main.height / 2;
+  init() {
+    const centerX = this.cameras.main.width / 2;
+    const centerY = this.cameras.main.height / 2;
 
-        // Create a container centered on the screen
-        this.container = this.add.container(centerX, centerY);
+    // Create a container centered on the screen
+    this.container = this.add.container(centerX, centerY);
 
-        // Progress bar elements relative to the container's center (0, 0)
-        const barWidth = this.cameras.main.width * 0.8;
-        const barHeight = 32;
+    // Progress bar elements relative to the container's center (0, 0)
+    const barWidth = this.cameras.main.width * 0.8;
+    const barHeight = 32;
 
-        // Outline rectangle centered at (0, 0) within the container
-        const progressBarOutline = this.add
-            .rectangle(0, 0, barWidth + 4, barHeight + 4)
-            .setStrokeStyle(1, 0xffffff);
-        this.container.add(progressBarOutline);
+    // Outline rectangle centered at (0, 0) within the container
+    const progressBarOutline = this.add.rectangle(0, 0, barWidth + 4, barHeight + 4).setStrokeStyle(1, 0xffffff);
+    this.container.add(progressBarOutline);
 
-        // Filling bar starting from the left edge relative to the container's center
-        const bar = this.add.rectangle(
-            -barWidth / 2, // Start from left edge relative to center
-            0, // Center vertically
-            4,
-            barHeight,
-            0xffffff
-        );
-        this.container.add(bar); // Add the bar itself to the container
+    // Filling bar starting from the left edge relative to the container's center
+    const bar = this.add.rectangle(
+      -barWidth / 2, // Start from left edge relative to center
+      0, // Center vertically
+      4,
+      barHeight,
+      0xffffff
+    );
+    this.container.add(bar); // Add the bar itself to the container
 
-        // Update bar width based on progress
-        this.load.on("progress", (progress: number) => {
-            bar.width = 4 + barWidth * progress;
-        });
-    }
+    // Update bar width based on progress
+    this.load.on("progress", (progress: number) => {
+      bar.width = 4 + barWidth * progress;
+    });
+  }
 
-    preload() {
-        //  Load the assets for the game
-        this.load.setPath("assets");
+  preload() {
+    //  Load the assets for the game
+    this.load.setPath("assets");
 
-        // Load background assets
-        this.load.image("bg", "game/bg/bg.png");
-        this.load.image("bg_walls", "game/bg/bg_walls.png");
-        this.load.image("bg_frame", "game/bg/bg_frame.png");
-        this.load.image("bg_decor", "game/bg/bg_decor.png");
+    // Load background assets
+    this.load.image("bg", "game/bg/bg.png");
+    this.load.image("bg_walls", "game/bg/bg_walls.png");
+    this.load.image("bg_frame", "game/bg/bg_frame.png");
+    this.load.image("bg_decor", "game/bg/bg_decor.png");
 
-        // Load container assets
-        this.load.image("container", "game/elements/container.png");
-        this.load.json("containerPhysics", "game/elements/container.json");
+    // Load container assets
+    this.load.image("container", "game/elements/container.png");
+    this.load.json("containerPhysics", "game/elements/container.json");
 
-        // Load cauldron resources
-        this.load.image("cauldron", "game/elements/cauldron.png");
-        this.load.json("cauldronPhysics", "game/elements/cauldron.json");
+    // Load cauldron resources
+    this.load.image("cauldron", "game/elements/cauldron.png");
+    this.load.image("cauldron_filled", "game/elements/cauldron_filled.png");
+    this.load.json("cauldronPhysics", "game/elements/cauldron.json");
 
-        // Load intake resources
-        this.load.image("intake", "game/elements/intake.png");
-        this.load.json("intakePhysics", "game/elements/intake.json");
+    // Load intake resources
+    this.load.image("intake", "game/elements/intake.png");
+    this.load.image("intake_filled", "game/elements/intake_filled.png");
+    this.load.json("intakePhysics", "game/elements/intake.json");
 
-        // Load furnace resource
-        this.load.image("furnace", "game/elements/furnace.png");
+    // Load furnace resource
+    this.load.image("furnace", "game/elements/furnace.png");
 
-        // Load pipe assets
-        this.load.image("pipe_back", "game/elements/pipe_back.png");
-        this.load.image("pipe_front", "game/elements/pipe_front.png");
+    // Load pipe assets
+    this.load.image("pipe_back", "game/elements/pipe_back.png");
+    this.load.image("pipe_front", "game/elements/pipe_front.png");
 
-        // Load loot details assets
-        this.load.atlas(
-            "loot-details-sprites",
-            "game/details/loot-details-sprites.png",
-            "game/details/loot-details-sprites.json"
-        );
+    // Load loot details assets
+    this.load.atlas("loot-details-sprites", "game/details/loot-details-sprites.png", "game/details/loot-details-sprites.json");
 
-        // Load junk details assets
-        this.load.atlas(
-            "junk-details-sprites",
-            "game/details/junk-details-sprites.png",
-            "game/details/junk-details-sprites.json"
-        );
-        this.load.json(
-            "junk-details-shapes",
-            "game/details/junk-details-shapes.json"
-        );
+    // Load junk details assets
+    this.load.atlas("junk-details-sprites", "game/details/junk-details-sprites.png", "game/details/junk-details-sprites.json");
+    this.load.json("junk-details-shapes", "game/details/junk-details-shapes.json");
 
-        // Claw
-        this.load.image("claw_anchor", "game/elements/claw_anchor.png");
+    // Claw
+    this.load.image("claw_anchor", "game/elements/claw_anchor.png");
 
-        this.load.atlas(
-            "clawParts", // Load claw parts atlas
-            "game/elements/claw-parts-sprites.png",
-            "game/elements/claw-parts-sprites.json"
-        );
+    this.load.atlas(
+      "clawParts", // Load claw parts atlas
+      "game/elements/claw-parts-sprites.png",
+      "game/elements/claw-parts-sprites.json"
+    );
 
-        this.load.json("clawPhysics", "game/elements/claw-parts-shapes.json"); // Load claw physics
-    }
+    this.load.json("clawPhysics", "game/elements/claw-parts-shapes.json"); // Load claw physics
+  }
 
-    create() {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
-        //  For example, you can define global animations here, so we can use them in other scenes.
+  create() {
+    //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+    //  For example, you can define global animations here, so we can use them in other scenes.
 
-        //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start("MainMenu");
-    }
+    //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
+    this.scene.start("MainMenu");
+  }
 }
