e0feaa5e77a8fa4734f230fa8fe3d36de271dab6
Omhet <omhetehmo@yandex.ru>
2025-04-23 19:59:37 +0200

    Update junk details and add new items
    - Modified junk-details-sprites.json to update sprite dimensions and filenames for various junk items.
    - Updated junk-details-sprites.png to reflect changes in the sprite sheet.
    - Enhanced junk.ts by adding new junk pieces including "Log", "Dead Fish", "Horse Shoe", "Golden Ring", "Golden Cup", "Golden Necklace", and others.
    - Adjusted properties such as rarity, durability, sell price, and temperature coefficient for existing and new junk items.
    - Removed outdated junk pieces and replaced them with new items to improve gameplay variety.

--- src/lib/craft/config/junk/junk.ts
+++ src/lib/craft/config/junk/junk.ts
@@ -10,81 +10,10 @@ registerJunkPiece("bone", {
   temperatureCoefficient: 0.5,
 });
 
-registerJunkPiece("basic_short_sword_blade", {
-  suitableForRecipeDetails: [RecipeDetailType.ShortSwordBlade],
-  name: "Basic Short Sword Blade",
-  rarity: Rarity.Common,
-  durability: Durability.Medium,
-  sellPriceCoefficient: 1,
-  temperatureCoefficient: 1,
-});
-
-registerJunkPiece("curved_silver_short_sword_blade", {
-  suitableForRecipeDetails: [RecipeDetailType.ShortSwordBlade],
-  name: "Curved Silver Short Sword Blade",
-  rarity: Rarity.Uncommon,
-  durability: Durability.High,
-  sellPriceCoefficient: 1.5,
-  temperatureCoefficient: 1.5,
-});
-
-registerJunkPiece("basic_short_sword_grip", {
-  suitableForRecipeDetails: [RecipeDetailType.Grip],
-  name: "Basic Short Sword Grip",
-  rarity: Rarity.Common,
-  durability: Durability.Low,
-  sellPriceCoefficient: 1,
-  temperatureCoefficient: 1,
-});
-
-registerJunkPiece("tree", {
-  suitableForRecipeDetails: [RecipeDetailType.Grip],
-  name: "Tree Short Sword Grip",
-  rarity: Rarity.Uncommon,
-  durability: Durability.Low,
-  sellPriceCoefficient: 1.5,
-  temperatureCoefficient: 0.5,
-});
-
-registerJunkPiece("golden_with_emerald_pommel", {
-  suitableForRecipeDetails: [RecipeDetailType.Pommel],
-  name: "Golden With Emerald Short Sword Pommel",
-  rarity: Rarity.Epic,
-  durability: Durability.Low,
-  sellPriceCoefficient: 2,
-  temperatureCoefficient: 2,
-});
-
-registerJunkPiece("iron_ore", {
-  suitableForRecipeDetails: [RecipeDetailType.Guard],
-  name: "Iron Ore",
-  rarity: Rarity.Common,
-  durability: Durability.Medium,
-  sellPriceCoefficient: 1,
-  temperatureCoefficient: 1,
-});
-
-registerJunkPiece("silver_ore", {
-  suitableForRecipeDetails: [RecipeDetailType.Guard],
-  name: "Silver Ore",
-  rarity: Rarity.Uncommon,
-  durability: Durability.Medium,
-  sellPriceCoefficient: 1.5,
-  temperatureCoefficient: 1.5,
-});
-
-registerJunkPiece("gold_ore", {
-  suitableForRecipeDetails: [RecipeDetailType.Guard],
-  name: "Gold Ore",
-  rarity: Rarity.Rare,
-  durability: Durability.Low,
-  sellPriceCoefficient: 2,
-  temperatureCoefficient: 1,
-});
-
+// Fluff items
 registerJunkPiece("fluff-yellow", {
   suitableForRecipeDetails: [],
-  name: "Fluff",
+  name: "Yellow Fluff",
   rarity: Rarity.Common,
   durability: Durability.Low,
   sellPriceCoefficient: 1,
@@ -93,7 +22,7 @@ registerJunkPiece("fluff-yellow", {
 
 registerJunkPiece("fluff-blue", {
   suitableForRecipeDetails: [],
-  name: "Fluff",
+  name: "Blue Fluff",
   rarity: Rarity.Common,
   durability: Durability.Low,
   sellPriceCoefficient: 1,
@@ -102,18 +31,225 @@ registerJunkPiece("fluff-blue", {
 
 registerJunkPiece("fluff-green", {
   suitableForRecipeDetails: [],
-  name: "Fluff",
+  name: "Green Fluff",
   rarity: Rarity.Common,
   durability: Durability.Low,
   sellPriceCoefficient: 1,
   temperatureCoefficient: 1,
 });
 
-registerJunkPiece("banana", {
+registerJunkPiece("banana_peel", {
   suitableForRecipeDetails: [],
-  name: "Banana",
+  name: "Banana Peel",
   rarity: Rarity.Common,
   durability: Durability.Low,
   sellPriceCoefficient: 1,
   temperatureCoefficient: 1,
+});
+
+registerJunkPiece("log", {
+  suitableForRecipeDetails: [RecipeDetailType.Grip],
+  name: "Log",
+  rarity: Rarity.Common,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 0.8,
+  temperatureCoefficient: 0.5,
+});
+
+registerJunkPiece("dead_fish", {
+  suitableForRecipeDetails: [],
+  name: "Dead Fish",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.5,
+  temperatureCoefficient: 0.3,
+});
+
+registerJunkPiece("horse_shoe", {
+  suitableForRecipeDetails: [RecipeDetailType.Guard],
+  name: "Horse Shoe",
+  rarity: Rarity.Common,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.2,
+  temperatureCoefficient: 1.5,
+});
+
+registerJunkPiece("golden_ring", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Golden Ring",
+  rarity: Rarity.Rare,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 2,
+  temperatureCoefficient: 1.5,
+});
+
+registerJunkPiece("golden_cup", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Golden Cup",
+  rarity: Rarity.Rare,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 2.5,
+  temperatureCoefficient: 1.5,
+});
+
+registerJunkPiece("golden_necklace", {
+  suitableForRecipeDetails: [],
+  name: "Golden Necklace",
+  rarity: Rarity.Rare,
+  durability: Durability.Low,
+  sellPriceCoefficient: 2.5,
+  temperatureCoefficient: 1.5,
+});
+
+registerJunkPiece("silver_cup", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Silver Cup",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.8,
+  temperatureCoefficient: 1.3,
+});
+
+registerJunkPiece("silver_necklace", {
+  suitableForRecipeDetails: [],
+  name: "Silver Necklace",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Low,
+  sellPriceCoefficient: 1.8,
+  temperatureCoefficient: 1.3,
+});
+
+registerJunkPiece("lollipop", {
+  suitableForRecipeDetails: [],
+  name: "Lollipop",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.5,
+  temperatureCoefficient: 0.3,
+});
+
+registerJunkPiece("rope", {
+  suitableForRecipeDetails: [RecipeDetailType.Grip],
+  name: "Rope",
+  rarity: Rarity.Common,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 0.8,
+  temperatureCoefficient: 0.4,
+});
+
+registerJunkPiece("saw", {
+  suitableForRecipeDetails: [RecipeDetailType.ShortSwordBlade],
+  name: "Saw",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.5,
+  temperatureCoefficient: 1.2,
+});
+
+registerJunkPiece("bolt", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Bolt",
+  rarity: Rarity.Common,
+  durability: Durability.High,
+  sellPriceCoefficient: 0.7,
+  temperatureCoefficient: 1.2,
+});
+
+registerJunkPiece("shell", {
+  suitableForRecipeDetails: [RecipeDetailType.Guard],
+  name: "Shell",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.2,
+  temperatureCoefficient: 0.6,
+});
+
+registerJunkPiece("old_shoe", {
+  suitableForRecipeDetails: [],
+  name: "Old Shoe",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.3,
+  temperatureCoefficient: 0.4,
+});
+
+registerJunkPiece("golden_coin", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Golden Coin",
+  rarity: Rarity.Rare,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.8,
+  temperatureCoefficient: 1.5,
+});
+
+registerJunkPiece("silver_coin", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Silver Coin",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.3,
+  temperatureCoefficient: 1.2,
+});
+
+registerJunkPiece("mushroom", {
+  suitableForRecipeDetails: [],
+  name: "Mushroom",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.6,
+  temperatureCoefficient: 0.3,
+});
+
+registerJunkPiece("button", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Button",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.5,
+  temperatureCoefficient: 0.5,
+});
+
+registerJunkPiece("robo_hand", {
+  suitableForRecipeDetails: [RecipeDetailType.Grip],
+  name: "Robo Hand",
+  rarity: Rarity.Rare,
+  durability: Durability.High,
+  sellPriceCoefficient: 2.2,
+  temperatureCoefficient: 1.8,
+});
+
+registerJunkPiece("nut", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Nut",
+  rarity: Rarity.Common,
+  durability: Durability.High,
+  sellPriceCoefficient: 0.7,
+  temperatureCoefficient: 1.3,
+});
+
+registerJunkPiece("silver_ring", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Silver Ring",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.5,
+  temperatureCoefficient: 1.2,
+});
+
+registerJunkPiece("deer_hoof", {
+  suitableForRecipeDetails: [RecipeDetailType.Pommel],
+  name: "Deer Hoof",
+  rarity: Rarity.Uncommon,
+  durability: Durability.Medium,
+  sellPriceCoefficient: 1.2,
+  temperatureCoefficient: 0.7,
+});
+
+registerJunkPiece("empty_bottle", {
+  suitableForRecipeDetails: [RecipeDetailType.Grip],
+  name: "Empty Bottle",
+  rarity: Rarity.Common,
+  durability: Durability.Low,
+  sellPriceCoefficient: 0.5,
+  temperatureCoefficient: 0.8,
 });
